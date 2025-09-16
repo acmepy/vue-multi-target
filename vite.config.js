404f49943c906e52f7c0815e9c9cbe02174e9591
Hugo <hjgamarra@gmail.com>
2025-09-16 16:28:23 -0300

    assets generated

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,13 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
+//https://github.com/vite-pwa/vite-plugin-pwa/blob/main/examples/vue-router/vite.config.ts
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), VitePWA()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    VitePWA({ manifest: { name: 'vue-vite-pwa', short_name: 'vue-vite-pwa', lang: 'es' } }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
