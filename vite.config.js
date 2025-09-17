c36ff11c050e221e9cda88a5dd3ed4a5327045bb
Hugo <hjgamarra@gmail.com>
2025-09-17 09:33:15 -0300

    prettier

--- vite.config.js
+++ vite.config.js
@@ -8,11 +8,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 //https://github.com/vite-pwa/vite-plugin-pwa/blob/main/examples/vue-router/vite.config.ts
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-    VitePWA({ manifest: { name: 'vue-vite-pwa', short_name: 'vue-vite-pwa', lang: 'es' } }),
-  ],
+  plugins: [vue(), vueDevTools(), VitePWA({ manifest: { name: 'vue-vite-pwa', short_name: 'vue-vite-pwa', lang: 'es' } })],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
