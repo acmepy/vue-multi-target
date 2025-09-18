38e8b4904262f6d8dea5fbfa4e30769b5de90c72
Hugo <hjgamarra@gmail.com>
2025-09-18 08:12:59 -0300

    pwa ok en produccion

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,29 @@ import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 //https://github.com/vite-pwa/vite-plugin-pwa/blob/main/examples/vue-router/vite.config.ts
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), VitePWA({ manifest: { name: 'vue-vite-pwa', short_name: 'vue-vite-pwa', lang: 'es' } })],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    VitePWA({
+      manifest: {
+        name: 'vue-vite-pwa',
+        short_name: 'vue-vite-pwa',
+        lang: 'es',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
