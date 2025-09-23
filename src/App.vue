f98c80d25124bd36398d4f8a15ee089ff0653962
Hugo <hjgamarra@gmail.com>
2025-09-23 10:28:48 -0300

    add iconify

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import { Icon } from '@iconify/vue';
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
+    <!--img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" /-->
+    <Icon icon="devicon:vuejs" class="logo" width="125" height="125" aria-label="Vue logo" />
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
-
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
